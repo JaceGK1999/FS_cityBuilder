d1035bc18ff0e56bd21b4a7dfbdcecc9dd08f6bb
Jacegk1999 <Jacegk1999@gmail.com>
2022-01-26 09:50:54 -0800

    added js for reset button during lecture

--- app.js
+++ app.js
@@ -6,3 +6,13 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+
+//reset button stuff
+  const reset = document.getElementById(reset)
+
+reset.addEventListener('click'), ( => {
+  slogans = []
+  sloganList.textContent = '';
+})
