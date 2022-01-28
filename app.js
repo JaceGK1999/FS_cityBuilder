9ed102ccc800c9b2212d1573147cdf74ef31d629
Jacegk1999 <Jacegk1999@gmail.com>
2022-01-27 17:49:30 -0800

    added a choice counter

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
-
+import { createStatsString } from './utils.js';
 
 const climateSelect = document.getElementById('climate-select');
 const climateImage = document.getElementById('climate-img');
+const homeSelect = document.getElementById('home-select');
+const homeImage = document.getElementById('home-img');
+const areaSelect = document.getElementById('area-select');
+const areaImage = document.getElementById('area-img');
+const reportEl = document.getElementById('report');
 
-
-
-
-
-
-
+// let slogans = [];
+let climateCounter = 0;
+let homeCounter = 0;
+let areaCounter = 0;
 
 climateSelect.addEventListener('change', (e) => {
     const value = e.target.value;
     const image = `./assets/climate-${value}.jpg`;
     climateImage.src = image;
-    //climateCounter++;
+    climateCounter++;
+    displayStats();
 });
-  
 
+homeSelect.addEventListener('change', (e) => {
+    const value = e.target.value;
+    const image = `./assets/home-${value}.jpg`;
+    homeImage.src = image;
+    homeCounter++;
+    displayStats();
+});
 
+areaSelect.addEventListener('change', (e) => {
+    const value = e.target.value;
+    const image = `./assets/area-${value}.jpg`;
+    areaImage.src = image;
+    areaCounter++;
+    displayStats();
+});
 
+function displayStats() {
+    const statsString = createStatsString(climateCounter, areaCounter, homeCounter);
+    reportEl.textContent = statsString;
+}
 
-// let state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
-
-
-
-
-
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
 
 //reset button stuff
 //   const reset = document.getElementById(reset)
