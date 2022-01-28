8afa61ba42e60d80a392c6c1b933499265b12035
Jacegk1999 <Jacegk1999@gmail.com>
2022-01-27 18:22:52 -0800

    got a working test

--- test/example.test.js
+++ test/example.test.js
@@ -1,14 +1,23 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { createStatsString } from '../utils.js';
 
 const test = QUnit.test;
+
+test('Testing my string', (expect) => {
+    const string = `You have changed the climate type Mountain times, the home type Old times, and the area City times. And nobody can forget your city's classic catchphrases:`;
+
+    const actual = createStatsString('Mountain', 'City', 'Old');
+
+    expect.equal(actual, string);
+});
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
-    
-    //Act 
+
+    //Act
     // Call the function you're testing and set the result to a const
     const actual = true;
 
